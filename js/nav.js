8201cb50d1cb345741cbafa6252bb17d5a57448f
845968074 <845968074@qq.com>
2017-01-10 17:28:27 +0800

    new_nav

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,24 @@
-/**
- * Created by anfen on 17-1-10.
- */
-function setIs_press() {
-    let revise = document.getElementById('revise');
-    let submit=document.getElementById('submit');
-    let information = document.getElementsByClassName('user1 form-control');
-    for (let i of information) {
-        i.setAttribute('is_press', 'false')
-    }
-    revise.setAttribute('is_press','false');
-    submit.setAttribute('is_press','false');
+
+// 获取模态窗口
+var modal = document.getElementById('myModal');
+// 获取图片模态框，alt 属性作为图片弹出中文本描述
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementById("img01");
+for (let i of img) {
+    i.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    };
 }
-function isTrue(id) {
-    console.log("id: " + id);
-    let hasTrue = document.getElementById(id);
-     console.log("hasTrue: " + hasTrue.getAttribute('is_press'));
-    return hasTrue.getAttribute('is_press');
-}
-function changeIs_press(id) {
-    let change = document.getElementById(id);
-    return (change.setAttribute('is_press', 'true'));
-}
-function revise_Usr () {
-    let information = document.getElementsByClassName('user1 form-control');
-    for (let i of information) {
-        i.disabled=false
-    }
+// 获取 <span> 元素，设置关闭模态框按钮
+var span = document.getElementsByClassName("close")[0];
+
+// 点击 <span> 元素上的 (x), 关闭模态框
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+function reset() {
+    let revise=document.getElementById('money');
+    revise.readOnly= false
 }
